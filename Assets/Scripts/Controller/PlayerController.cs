23ebd6c34b15685ca07ee6da504e7f07aaee4741
Vinícius Assunção <vinicius.assuncao@dtidigital.com.br>
2020-02-01 09:16:04 -0300

    Added player

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController characterController;
+
+    public float speed = 5.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        moveDirection *= speed;
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
