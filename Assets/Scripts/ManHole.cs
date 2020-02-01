e7ff5057b750be0a53d0e6eb92f80586b6b3f558
Thales Bacelar Nascimento <c0007750@vli-logistica.com.br>
2020-01-31 22:27:14 -0300

    commit do plano

---
+++ Assets/Scripts/ManHole.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ManHole : MonoBehaviour
+{
+    public float drainage;
+
+    private WaterLevel waterLevel;
+    
+    void Start()
+    {
+        waterLevel = FindObjectOfType<WaterLevel>();
+    }
+    
+    void Update()
+    {
+        waterLevel.Drain(transform.position.z, drainage / Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/WaterLevel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class WaterLevel : MonoBehaviour
+{
+    public float riseSpeed;
+    public float level;
+
+    private Vector3 positionAtStart;
+
+    void Start()
+    {
+        positionAtStart = transform.position;
+    }
+    
+    void Update()
+    {
+        level += riseSpeed / Time.deltaTime;
+        transform.position = positionAtStart + level * Vector3.forward;
+    }
+
+    public void Drain(float minHeight, float value)
+    {
+        if (positionAtStart.z + level >= minHeight)
+        {
+            Debug.Log("drain " + value);
+            level = Mathf.Max(minHeight, level - value);
+        }
+    }
+}
