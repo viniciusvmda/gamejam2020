83ad82ef21fefcbb8e4e55ea825145dda48b74f6
renannascimento <renan.engcomp@gmail.com>
2020-02-02 10:58:04 -0300

    add: game menu and tuto

---
+++ Assets/Scripts/InitGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitGame : MonoBehaviour
+{
+    public List<GameObject> objectsDisabled;
+
+    void Update()
+    {
+        if (Input.GetButtonUp("Submit"))
+        {
+            foreach(GameObject o in objectsDisabled)
+            {
+                o.SetActive(true);
+            }
+
+            gameObject.SetActive(false);
+        }
+
+    }
+}
